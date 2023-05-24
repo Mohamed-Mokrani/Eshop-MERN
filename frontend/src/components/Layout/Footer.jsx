081c0648459f5594d6c7c7f82f6b7eea56034012
Mohamed-Mokrani <med.mokranii@hotmail.fr>
2023-05-24 23:31:20 +0100

    update

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -60,7 +60,20 @@ const Footer = () => {
           </div>
         </ul>
 
-        
+        <ul className="text-center sm:text-start">
+          <h1 className="mb-1 font-semibold">Company</h1>
+          {footerProductLinks.map((link,index) => (
+            <li key={index}>
+              <Link
+                className="text-gray-400 hover:text-teal-400 duration-300
+                   text-sm cursor-pointer leading-6"
+                to={link.link}
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
 
         <ul className="text-center sm:text-start">
           <h1 className="mb-1 font-semibold">Shop</h1>
