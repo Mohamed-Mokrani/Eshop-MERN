ca4b58e7bc5429fc6bf258f6bf64ded98957060d
Mohamed MOKRANI <119493044+Mohamed-Mokrani@users.noreply.github.com>
2023-05-27 14:18:44 +0100

    Update AdminDashboardEvents.jsx

--- frontend/src/pages/AdminDashboardEvents.jsx
+++ frontend/src/pages/AdminDashboardEvents.jsx
@@ -3,6 +3,7 @@ import AdminHeader from '../components/Layout/AdminHeader'
 import AdminSideBar from '../components/Admin/Layout/AdminSideBar'
 import AllEvents from '../components/Admin/AllEvents';
 
+
 const AdminDashboardEvents = () => {
   return (
     <div>
